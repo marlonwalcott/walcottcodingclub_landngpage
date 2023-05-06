5b74727e061588b6bf8c145bbe6553aeb9412d37
marlon walcott <marlonwalcott@gmail.com>
2023-05-06 13:05:28 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Footer from "./components/Footer";
 import "./styles.css";
 
 export default function App() {
+  const language = "en"; // Set the language you want to use (e.g., 'en', 'es', 'fr', etc.)
+
   return (
     <div className="App">
-      <Header />
+      <Header language={language} />
+
       <Footer />
     </div>
   );
---
+++ src/Global/Constants.js
@@ -0,0 +1,7 @@
+// Global/Constants.js
+export const WEBSITENAME = {
+  'en': 'Walcott Coding Club',
+  'es': 'Club de Programación Walcott',
+  'fr': 'Club de Programmation Walcott',
+  // ... add more languages as needed
+};
