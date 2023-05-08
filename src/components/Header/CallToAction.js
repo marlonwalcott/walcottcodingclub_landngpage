f3878193cd6c658ee61614fcd83a49b1a90591bc
marlon walcott <marlonwalcott@gmail.com>
2023-05-08 20:22:33 +0000

    Update 4 files

--- src/components/Header/CallToAction.js
+++ src/components/Header/CallToAction.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import "./Header.css";
+import "./HeaderMobile.css";
 
 class CallToAction extends Component {
   render() {
     return (
       <div className="navbar-text">
-        <i class="fa fa-search" aria-hidden="true"></i>
-        <a href="http://walcottcodingclub.com/signup/index.html">
-          Free Trial 
-        </a>
-          <a href="http://walcottcodingclub.com/signup/index.html">
-         Enrol
-        </a>
+        <div className="calltoactioncall floatleftcall paddingcall">
+        <i class="fa fa-search darkfontcall" aria-hidden="true"></i>
+        </div>
+        <div className="calltoactioncall floatleftcall paddingcall roundcornercall lightroundcall ">
+          <a  className="whitefontcall paddingcall" href="http://walcottcodingclub.com/signup/index.html">
+            Free Trial
+          </a>
+        </div>
+          <div className="calltoactioncall darkbackroundcall floatleftcall paddingcall roundcornercall margincall  ">
+        <a  className="whitefontcall"  href="http://walcottcodingclub.com/signup/index.html">Enrol</a>
       </div>
+       </div>
     );
   }
 }
